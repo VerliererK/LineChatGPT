808adfa2ee3938e74c0e063085327df401481aea
VerliererK <r03548014@ntu.edu.tw>
2023-04-01 15:09:21 +0800

    Decrease default timeout of openapi, prevent firestore timeout when messages is big.

--- config/config.ts
+++ config/config.ts
@@ -2,7 +2,7 @@ export const CONFIG = {
   API_HOST: process.env.API_HOST ?? "",
   OPENAI_SYSTEM_ROLE: process.env.OPENAI_SYSTEM_ROLE ?? null,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? "",
-  OPENAI_API_TIMEOUT: Number(process.env.OPENAI_API_TIMEOUT) || 29000,
+  OPENAI_API_TIMEOUT: Number(process.env.OPENAI_API_TIMEOUT) || 28000,
   OPENAI_TOP_P: Number(process.env.OPENAI_TOP_P) || 0.3,
   OPENAI_MAX_TOKENS: Number(process.env.OPENAI_MAX_TOKENS) || 200,
   OPENAI_PRESENCE_PENALTY: Number(process.env.OPENAI_PRESENCE_PENALTY) || 0.6,
